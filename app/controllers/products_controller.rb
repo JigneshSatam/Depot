9e817a07fdb67b53ebea1fcfeb5a9608fc696f72
JigneshSatam <jignesh.satam@kiprosh.com>
2014-07-24 18:41:21 +0530

    Validations, Unit testing, Functional Testing

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,33 +1,46 @@
 class ProductsController < ApplicationController
+  before_action :get_product, except:[:index, :new, :create]
   def index
     @products = Product.all
   end
+
   def new
     @product = Product.new
   end
+
   def create
     @product = Product.new(add_params)
-    @product.save
-    redirect_to product_path(@product)
+    if @product.save
+      redirect_to product_path(@product)
+    else
+      render template: "products/new"
+    end
   end
+
   def destroy
-    @product = Product.find(params[:id])
     @product.destroy
     redirect_to products_path
   end
+
   def show
-    @product = Product.find(params[:id])
   end
+
   def edit
-    @product = Product.find(params[:id])
   end
+
   def update
-    @product = Product.find(params[:id])
-    @product.update(add_params)
-    redirect_to product_path(@product)
+    if @product.update(add_params)
+      redirect_to product_path(@product)
+    else
+      render template: "products/edit"
+    end
   end
+
   private
   def add_params
     params.require(:product).permit(:title, :description, :image_url, :price)
+  end
+  def get_product
+    @product = Product.find(params[:id])
   end
 end
--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,7 +1,50 @@
 require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
+  fixtures :products
   # test "the truth" do
   #   assert true
   # end
+  test "Product attributes must not be Empty" do
+    product = Product.new
+    assert product.invalid?
+    assert product.errors[:title].any?
+    assert product.errors[:description].any?
+    assert product.errors[:price].any?
+    assert product.errors[:image_url].any?
+  end
+  test "Product price should be Positive" do
+    product = Product.new(title:'My book',description:'yyy', image_url:'zzz.jpg')
+    product.price = -1
+    assert product.invalid?
+    assert_equal "must be greater than or equal to 0.01", product.errors[:price].join("; ")
+
+    product.price = 0
+    assert_equal "must be greater than or equal to 0.01", product.errors[:price].join("; ")
+    product.price = 1
+    assert product.valid?
+  end
+
+  def new_product(image = "fred.gif")
+    Product.new(title:'My book',description:'yyy', image_url: image, price: 1)
+  end
+
+  test "image_url" do
+    ok = %w{ fred.gif fred.jpg fred.png FRED.JPG FRED.Jpg http://a.b.c/x/y/z/fred.gif }
+    bad = %w{ fred.doc fred.gif/more fred.gif.more }
+    ok.each do |name|
+      assert new_product(name).valid?, "#{name} should be valid"
+    end
+
+    bad.each do |name|
+      assert new_product(name).invalid?, "#{name} shouldn't be valid"
+    end
+  end
+
+  test "product is not valid without a unique title" do
+    product = new_product()
+    product.title = products(:ruby).title
+    assert !product.save
+    assert_equal I18n.translate('activerecord.errors.messages.taken'), product.errors[:title].join('; ')
+  end
 end
